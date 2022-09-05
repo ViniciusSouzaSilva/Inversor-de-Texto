8c8a4f8cb9517f2fdff8a639d632d904c6b064e7
Vinicius de Souza Silva <105164572+ViniciusSouzaSilva@users.noreply.github.com>
2022-09-05 12:33:00 -0300

    v1.0
    Primeira versão estável da aplicação
    
    Inclui:
    
    - 2 funções diferentes para inversão do texto;
    - função de copiar o texto invertido
    - estilização básica da página

---
+++ script.js
@@ -0,0 +1,42 @@
+function inverterCompleto()
+{
+    var txt1 = document.getElementById('norm')
+    var txt2 = document.getElementById('inv')
+    var str = txt1.value
+    var nstr = []
+    str = str.split('')
+    str.reverse()
+    str = str.join('')
+    str = str.split(' ')
+    str = str.join(' ')
+
+    txt2.value = str
+}
+
+function inverterPalavras()
+{
+    var txt1 = document.getElementById('norm')
+    var txt2 = document.getElementById('inv')
+    var str = txt1.value
+    var nstr = []
+    str = str.split('')
+    str.reverse()
+    str = str.join('')
+    str = str.split(' ')
+
+    for(let i = str.length - 1; i >= 0; i--)
+    {
+        nstr.push(str[i])
+    }
+
+    nstr = nstr.join(' ')
+
+    txt2.value = nstr
+}
+
+function copiar()
+{
+    var txt2 = document.getElementById('inv')
+    txt2.select()
+    document.execCommand('copy')
+}
